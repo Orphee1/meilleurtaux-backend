2ce97b3c820c03b9a7289dae1ca6fec645559b50
HugoLattard <55689599+Orphee1@users.noreply.github.com>
2019-12-21 23:58:18 +0100

    log-in with token

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const uid2 = require("uid2");
 
 // READ ==========================================================
-router.post("/log-in", (req, res) => {
+router.post("/log-in", async (req, res) => {
   console.log("Login OK");
-  res.json({ message: "Login OK" });
+
+  const user = {
+    token: uid2(64)
+  };
+
+  try {
+    let password = req.body.password;
+    console.log(password);
+
+    console.log(user.token);
+    if (password === "tothemoon") {
+      res.status(200).json(user.token);
+      console.log("Authentification OK");
+    } else {
+      res.json({ message: "authentification failed" });
+      console.log("Authentification failed");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
